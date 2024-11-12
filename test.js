d28618ff59b790006fe14cffd954100ddb51d30b
zevilz <zacharygriffee@gmail.com>
2024-11-12 03:40:23 -0500

    bump rxprotoplex, modify test to show best cleanup practices.

--- test.js
+++ test.js
@@ -1,47 +1,61 @@
 import {test} from "brittle";
-import {createPlexPair} from "rxprotoplex";
+import {createPlexPair, destroy} from "rxprotoplex";
 import {listenAndConnectionAndRpc$} from "./lib/listenAndConnectionAndRpc$.js";
 import {connectAndRpc$} from "./lib/connectAndRpc$.js";
-import {finalize, from, map, switchMap, tap} from "rxjs";
+import {finalize, from, map, switchMap, take, takeUntil, tap, timer} from "rxjs";
 import {switchRpcRequest} from "./lib/switchRpcRequest.js";
 import {tapExpose} from "./lib/tapExpose.js";
 
-test("add two numbers over rpc", t => {
-    t.plan(6);
+test("math over rpc", t => {
+    t.plan(7);
     const [p1, p2] = createPlexPair();
 
-    p1.mux.stream.once("close", () => t.pass());
-    p2.mux.stream.once("close", () => t.pass());
+    p1.close$.subscribe(() => t.pass());
+    p2.close$.subscribe(() => t.pass());
 
-    const rpcServer = listenAndConnectionAndRpc$(p1)
+    listenAndConnectionAndRpc$(p1)
         .pipe(
             finalize(() => t.pass("rpc server completes.")),
+            takeUntil(timer(100)),
             tapExpose({
                 add(a, b) {
                     return a + b;
+                },
+                mul(a, b) {
+                    return a * b;
                 }
-            })
+            }),
         )
         .subscribe();
 
-    const rpcClient = connectAndRpc$(p2).pipe(
+    connectAndRpc$(p2).pipe(
         finalize(() => t.pass("rpc client completes.")),
         switchRpcRequest("add", 5, 6),
-        tap(({rpc}) => rpc.stream.destroy())
+        take(1)
     ).subscribe(
-        ({ack: sum, rpc}) => {
-            t.is(sum, 11);
-            t.ok(rpc.stream.destroying || rpc.stream.destroyed);
-            fin();
+        {
+            next: ({ack: sum}) => {
+                t.is(sum, 11);
+            }
         }
     );
 
-    // streams do not close when subscription ends, you still need to clean them up
-    function fin() {
-        rpcServer.unsubscribe();
-        rpcClient.unsubscribe();
+    connectAndRpc$(p2).pipe(
+        finalize(() => t.pass("rpc client completes.")),
+        switchRpcRequest("mul", 5, 6),
+        take(1)
+    ).subscribe(
+        {
+            next: ({ack: mul}) => {
+                t.is(mul, 30);
+            }
+        }
+    );
 
-        p1.mux.stream.end();
-        p2.mux.stream.end();
-    }
+    setTimeout(() => {
+        destroy(p1);
+        destroy(p2);
+    }, 500);
 });
+
+
